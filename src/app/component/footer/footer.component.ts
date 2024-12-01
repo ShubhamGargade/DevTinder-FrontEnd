c65331362f4a92dad8b8ff9f1c8819a83fb6f5ed
Shubham Gargade <gargadeshubham@gmail.com>
2024-12-01 14:14:11 +0530

    Initial Code setup for Dev Tinder application.

---
+++ src/app/component/footer/footer.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss'
+})
+export class FooterComponent {
+  new: any;
+  copyRightCurrentYear: number = new Date().getFullYear();
+}
